b07f1c9da72ec4f177ea49e228267cb26adb2638
thanhtungpfiev <thanhtungpfiev@gmail.com>
2022-06-12 17:47:19 +0700

    Finish chapter 15 v2

--- Chapter15/05/restful05/toys/views.py
+++ Chapter15/05/restful05/toys/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
 from rest_framework import status
 from toys.models import Toy
 from toys.serializers import ToySerializer
